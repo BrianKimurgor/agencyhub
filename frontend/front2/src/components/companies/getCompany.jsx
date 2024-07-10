3bfe4c3e3b332cd7dd00c71253bbddd7207d781e
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-07-10 16:05:00 +0300

    finished testing comany endpoint, UI is remainingfor comany.

--- frontend/front2/src/components/companies/getCompany.jsx
+++ frontend/front2/src/components/companies/getCompany.jsx
@@ -1,52 +1,18 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from 'react';
+import React, {  } from 'react';
 import CompanyForm from './companyForm';
 import CompanyList from './companyList';
-import { companes } from '../../services/companyService';
 
-const GetCompanyController = () => {
-    const [companies, setCompanies] = useState([]);
-    const [editingCompany, setEditingCompany] = useState({
-        id: 0,
-        name: '',
-        address: '',
-        contactEmail: '',
-        contactPhone: '',
-        industry: ''
-    });
+const GetCompanyController = ({handleDeleteCompany, editingCompany, handleUpdateCompany, companies, handleEditCompany}) => {
 
-    useEffect(() => {
-        companes.getCompanies().then(data => setCompanies(data));
-    }, []);
 
-    const handleCreateCompany = (formData) => {
-        companes.createCompany(formData).then(newCompany => {
-            setCompanies([...companies, newCompany]);
-        });
-    };
-
-    const handleUpdateCompany = (formData) => {
-        companes.updateCompany({...formData, id: editingCompany.id}).then(updatedCompany => {
-            setCompanies(companies.map(company => company.id === updatedCompany.id ? updatedCompany : company));
-        });
-    };
-
-    const handleDeleteCompany = (companyId) => {
-        companes.deleteCompany(companyId).then(() => {
-            setCompanies(companies.filter(company => company.id !== companyId));
-        });
-    };
-
-    const handleEditCompany = (company) => {
-        setEditingCompany(company);
-        console.log(company)
-    };
 
     return (
         <div>
             <h1>Company Management</h1>
             <CompanyForm
-                onSubmit={editingCompany ? handleUpdateCompany : handleCreateCompany}
+                onSubmit={(formData) => handleUpdateCompany(formData) }
                 initialData={editingCompany}
             />
             <CompanyList
