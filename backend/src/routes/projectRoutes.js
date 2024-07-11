4a8303f03851244801e5b1b9b2ea0edb0231c103
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-07-11 17:50:31 +0300

    created project endpoint, updated the api, styled with bootsttrap a little, everything works on project endpoint.

--- backend/src/routes/projectRoutes.js
+++ backend/src/routes/projectRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createProject, getProject, updateProject, deleteProject } from '../controllers/projectController.js';
+import { createProject, getProject, updateProject, deleteProject, getProjects } from '../controllers/projectController.js';
 
 
 const router = express.Router();
 
 router.post('/projects', createProject);
+router.get('/projects', getProjects)
 router.get('/projects/:id', getProject);
 router.put('/projects/:id', updateProject);
 router.delete('/projects/:id', deleteProject);
